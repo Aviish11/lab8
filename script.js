193d642f9b13890ae711ae8512383b5791702ab6
Auckloo Veevek <avish@Auckloos-MacBook-Pro.local>
2024-11-29 04:32:38 -0500

    updated displayInfo function to include log and called function

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ const student = {
     courses: ["WebDev", "Algo", "JavaScript"],
 
     displayInfo: function() {
-        return `Student Name: ${this.name}, Age: ${this.age}, Courses: ${this.courses}`;
+        console.log(`Name: ${this.name}, Age: ${this.age}, Courses: ${this.courses}`);
+        
     }
 };
 
 console.log("Name:", student.name);
 console.log("Age:", student.age);
+
+student.displayInfo();
